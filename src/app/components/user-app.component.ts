2859450dc7b741c4bb13cd55fce441edd48fac9d
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-11-21 14:29:15 -0500

    adelanto

---
+++ src/app/components/user-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-app.component.html'
+})
+export class UserAppComponent {
+
+  title: string = 'listado de usuarios!'
+
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private users: User[] = [{
+    id: 1,
+    name: 'leonard',
+    lastname: 'hernandez',
+    email:'leonard@example.com',
+    username:'leonardUser',
+    password:'123456'
+  },
+  {
+    id: 2,
+    name: 'lubraska',
+    lastname: 'ferreira',
+    email:'lubraska@example.com',
+    username:'lubraskaUser',
+    password:'123456'
+  },{
+    id: 3,
+    name: 'andres',
+    lastname: 'guzman',
+    email:'andres@example.com',
+    username:'andresUser',
+    password:'123456'
+  }]
+
+  constructor() { }
+
+  findAll(): Observable<User[]> {
+    return of()
+  }
+}
