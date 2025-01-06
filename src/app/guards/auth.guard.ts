b99c71f770d8e1bd9ba0f988a4be78bbb34c9c2a
Leonard <Leonardhernandez02@gmail.com>
2025-01-05 21:15:27 -0500

    creando componente forbiden y arreglando detalles de diseño

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,33 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  inject(Router).navigate(['/login']);
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.authenticated()) {
+    if (isTokenExpired()) {
+      authService.logout();
+      router.navigate(['/login']);
+      return false
+    }
+    if (!authService.isAdmin()) {
+      router.navigate(['/forbidden']);      
+    }
+    return true;    
+  }
+
+  router.navigate(['/login']);
   return false;
 };
+
+const isTokenExpired = (): boolean => {
+  const authService = inject(AuthService);
+  const token = authService.token;
+  const payload = authService.getPayload(token);
+  const exp = payload.exp;
+  const now = Date.now() / 1000;
+  return now > exp;
+}
